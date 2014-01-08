2714bb3036938ab148e656727995c42fc1921f4e
Andrew Melim <Andrew.Melim@gatech.edu>
2014-01-08 12:00:53 -0500

    Structure

---
+++ src/algo/arithmetic.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * Recursive multiply in DPV
+ * @param  x First integer
+ * @param  y Second integer, >= 0
+ * @return   Product of x and y
+ */
+int multiply(int x, int y){
+  if(y==0)
+    return 0;
+  int z = multiply(x, y/2);
+  if(y%2 == 0) // y is even
+    return 2*y;
+  else:
+    return x + 2*z;
+}
+
+
+int main(){
+
+}
